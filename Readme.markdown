332c1de4f10ddf073fbb894f460f5f9a933e2b86
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-11-22 14:27:33 +0100

    First version

---
+++ Readme.markdown
@@ -0,0 +1,37 @@
+Collectd-influxdb-proxy
+---
+
+Small proxy between collectd and influxdb.
+
+On collectd side, add following plugin
+
+    LoadPlugin "write_http"
+    <Plugin "write_http">
+      <URL "http://10.0.0.130:8079">
+        Format "JSON"
+      </URL>
+    </Plugin>
+
+Where 10.0.0.130:8079 is the listening point of your collectd-influxdb proxy.
+
+To start the proxy
+
+    node proxy.js --influxdb_db mydb --influxdb_user myuser --influxdb_password mypassword
+
+Where mydb, myuser and myspassword are connection parameter to Influxdb. By default, collectd-influxb-proxy assume influxdb is deployed on the same server.
+
+Other collectd-influxdb-proxy :
+
+    node proxy.js [options]
+    options :
+      --proxy_http_port port : proxy http port, default value 8079
+      --proxy_http_address address : proxy http address, default value 0.0.0.0
+      --influxdb_host : influxdb host, default value localhost
+      --influxdb_port : influxdb port, default value 8086
+      --influxdb_db : influxdb db
+      --influxdb_user : influxdb user
+      --influxdb_password : influxdb password
+      --verbose : display metric name pushed into influxdb
+      --help : this help
+
+Note : only ``gauge`` and ``counter`` metrics from Collectd are processed, and are transmitted as is to Influxdb.
---
+++ proxy.js
@@ -0,0 +1,117 @@
+
+var http = require('http'),
+    argv = require('optimist').argv;
+
+function usage() {
+  console.log('node proxy.js [options]');
+  console.log('options :');
+  console.log('  --proxy_http_port port : proxy http port, default value 8079');
+  console.log('  --proxy_http_address address : proxy http address, default value 0.0.0.0');
+  console.log('  --influxdb_host : influxdb host, default value localhost');
+  console.log('  --influxdb_port : influxdb port, default value 8086');
+  console.log('  --influxdb_db : influxdb db');
+  console.log('  --influxdb_user : influxdb user');
+  console.log('  --influxdb_password : influxdb password');
+  console.log('  --verbose : display metric name pushed into influxdb');
+  console.log('  --help : this help');
+  process.exit(1);
+}
+
+if (argv.help) {
+  usage();
+}
+
+if (!argv.proxy_http_port) {
+  argv.proxy_http_port = 8079;
+}
+
+if (!argv.proxy_http_address) {
+  argv.proxy_http_address = '0.0.0.0';
+}
+
+if (!argv.influxdb_host) {
+  argv.influxdb_host = 'localhost';
+}
+
+if (!argv.influxdb_port) {
+  argv.influxdb_port = '8086';
+}
+
+if (!argv.influxdb_db) {
+  console.log('Missing param : influxdb_db');
+  usage();
+}
+
+if (!argv.influxdb_user) {
+  console.log('Missing param : influxdb_user');
+  usage();
+}
+
+if (!argv.influxdb_password) {
+  console.log('Missing param : influxdb_password');
+  usage();
+}
+
+argv.influxdb_path = '/db/' + argv.influxdb_db + '/series?u=' + argv.influxdb_user + '&p=' + argv.influxdb_password;
+console.log('Influx db config');
+console.log('Host :', argv.influxdb_host, ':', argv.influxdb_port);
+console.log('Path :', argv.influxdb_path);
+
+var server = http.createServer(function(req, res) {
+  var data = '';
+  req.on('data', function(chunk) {
+    data = data + chunk.toString();
+  });
+  req.on('end', function() {
+    res.writeHead(200);
+    res.end();
+
+    var output = [];
+    var parsed = JSON.parse(data);
+    parsed.forEach(function(x) {
+      var name = x.host + '.' + x.plugin;
+      if (x.plugin_instance !== '') {
+        name = name + '.' + x.plugin_instance;
+      }
+      name = name + '.' + x.type;
+      if (x.type_instance !== '') {
+        name = name + '.' + x.type_instance;
+      }
+      for(var z in x.dstypes) {
+        if (x.dstypes[z] == 'counter' || x.dstypes[z] == 'gauge') {
+          var n = name + '.' + x.dsnames[z];
+          if (argv.verbose) {
+            console.log('Push metric', n);
+          }
+          output.push({
+            name: n,
+            columns: ['time', 'value'],
+            points: [[x.time, x.values[z]]],
+          });
+        }
+      }
+    });
+    var forwarded_req = {
+      hostname: argv.influxdb_host,
+      port: argv.influxdb_port,
+      path: argv.influxdb_path,
+      method: 'POST'
+    };
+    var r = http.request(forwarded_req, function(rr) {
+      if (rr.statusCode != "200") {
+        console.error('Request refused by influx db', rr.statusCode);
+      }
+    });
+
+    r.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+    r.write(JSON.stringify(output));
+    r.end();
+  });
+});
+
+server.listen(argv.proxy_http_port, argv.proxy_http_address);
+
+console.log('Proxy started on port', argv.proxy_http_port, 'ip', argv.proxy_http_address);
+
